338dba668e24e82e03b818e0780cb32fcd253917
iev <iev@land.ru>
2014-06-03 16:51:14 +0400

    test commit

--- commit.sh
+++ commit.sh
@@ -53,7 +53,8 @@ commit()
         (cd $i && git add ./* && git commit -m "$msg" && cd ..)
     done
     git add ./* && git commit -m "$msg"
-    git push --recurse-submodules=on-demand origin HEAD:master
+    #git push --recurse-submodules=on-demand origin HEAD:master
+    git push --recurse-submodules=on-demand
 }
 
 case $1 in
