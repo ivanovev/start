975b8b25c79e99555b48b7ffafde09c3e3ac5a8b
iev <iev@land.ru>
2014-06-11 20:34:38 +0400

    added starthm, startfio

--- startall.py
+++ startall.py
@@ -3,7 +3,7 @@
 import util.startup
 
 def main():
-    all_submodules = ['hm', 'misc', 'ctl']
+    all_submodules = ['hm', 'misc', 'ctl', 'fio']
     util.startup.startup(*all_submodules)
 
 if __name__ == '__main__':
